3f7e097f306dc9c81e205382e23616c3240e5a5f
JonasKoller <jonas@CSSs-MacBook-Pro-2.local>
2018-03-26 09:48:47 +0200

    Projekt aufsetzen

--- app/Views/welcome.view.php
+++ app/Views/welcome.view.php
@@ -1,14 +1,20 @@
 <!DOCTYPE html>
 <html lang="de">
 <head>
-    <meta charset="UTF-8">
-    <title>Meine Seite</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- Custom CSS -->
     <link rel="stylesheet" href="public/css/app.css">
+
+    <title>Kredihay - Home</title>
 </head>
 <body>
 
-<h1 class="welcome">Willkommen im 307-Framework!</h1>
+<h1>Hello, world!</h1>
 
-<script src="public/js/app.js"></script>
 </body>
 </html>
